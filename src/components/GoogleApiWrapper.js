429dcdfb7ee864e9a923a9931eca407790110d52
Konrad Kowalski <konradkowalski13@gmail.com>
2018-01-31 22:18:51 +0000

    info window

--- src/components/GoogleApiWrapper.js
+++ src/components/GoogleApiWrapper.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
+import { render } from 'react-dom';
+
 import {InfoWindow, Marker, GoogleApiWrapper, withScriptjs, GoogleMap} from 'google-maps-react';
 import GoogleMapDrawFilter from "react-google-map-draw-filter";
+
+class DetailsForm extends Component {
+  onFormSubmit(event) {
+    event.preventDefault();
+    console.log(event.target.elements.details.value);
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={ this.onFormSubmit.bind(this) }>
+          <input name='details' placeholder='Description' />
+          <input type='submit' value='Submit' />
+        </form>
+      </div>
+    )
+  }
+}
+
 
 export class MapContainer extends Component {
 //   constructor () {
@@ -133,8 +154,18 @@ addMarker(label, info, lat, long) {
 }
 
 onMarkerClick(marker,e){
+  const infowindow = new google.maps.InfoWindow({
+    content: `<div id='details'></div>`
+  });
+
+  google.maps.event.addListener(infowindow, 'domready', function() {
+    render(<DetailsForm />, document.getElementById('details'));
+  });
+
+  infowindow.open(marker.map, marker);
+
   this.setState({
-    activeMarkers : [markers]
+    activeMarkers : [marker]
   });
 }
 
@@ -187,7 +218,7 @@ handleSubmit(ev) {
       lng: results[0].geometry.location.lng()
     };
 
-    this.addMarker('LABEL', 'more info', location.lat, location.lng);
+    this.addMarker('Listing', '', location.lat, location.lng);
   });
 
 
@@ -216,39 +247,38 @@ render() {
   }
 
   const markers = [
-      {
-        info:'- Marker1',
-        label:'A',
-        title:'hello',
-        latLng:{lng:-123.097665,lat:49.264254}
-      },
-
-      {
-        label:'B',
-        info:'- Marker2',
-        latLng:{lng:-123.104124,lat: 49.266416}
-      },
-
-      {
-        label:'C',
-        info:'- Marker 3',
-        latLng:{lng:-123.117943 ,lat:49.278747}
-      },
-
       // {
-      //   label:'D',
-      //   info:'- Marker 4',
-      //   latLng:{lng:-123.126311 ,lat:49.278988}
+      //   info:'- Marker1',
+      //   label:'A',
+      //   title:'hello',
+      //   latLng:{lng:-123.097665,lat:49.264254}
+      // },
+      //
+      // {
+      //   label:'B',
+      //   info:'- Marker2',
+      //   latLng:{lng:-123.104124,lat: 49.266416}
+      // },
+      //
+      // {
+      //   label:'C',
+      //   info:'- Marker 3',
+      //   latLng:{lng:-123.117943 ,lat:49.278747}
+      // },
+      //
+      // // {
+      // //   label:'D',
+      // //   info:'- Marker 4',
+      // //   latLng:{lng:-123.126311 ,lat:49.278988}
+      // // },
+      //
+      // {
+      //   label:'E',
+      //   info:'- Marker 5',
+      //   latLng:{lng:-123.132362 ,lat:49.275147}
       // },
 
-      {
-        label:'E',
-        info:'- Marker 5',
-        latLng:{lng:-123.132362 ,lat:49.275147}
-      },
-
     ];
-
 
     return (<div>
       <button onClick={this.toggleDraw.bind(this)}>Polygon Search</button>
@@ -266,14 +296,19 @@ render() {
         />
 
       </div>
+
       <h1>{this.renderMarkerInfo.bind(this)()}</h1>
 
       <form onSubmit={this.handleSubmit.bind(this)}>
         <label>
             Address:
           <input type="text" value={this.state.value} onChange={this.handleChange.bind(this)} />
+            Description
+          <input type="text" />
+
         </label>
         <input type="submit" value="Submit" onClick={this.handleSubmit.bind(this)}/>
+
       </form>
     </div>
   );

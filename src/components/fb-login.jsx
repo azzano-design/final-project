3fb863de2b8cb90646026a0e00ad88e530d4d5b5
Arthur Di Lascio <arthur.dilascio@gmail.com>
2018-02-01 04:32:35 +0000

    facebook login bug fixe

--- src/components/fb-login.jsx
+++ src/components/fb-login.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class FacebookLogin extends Component {
   constructor() {
@@ -8,7 +8,22 @@ class FacebookLogin extends Component {
     }
   }
 
-  facebookInit() {
+
+  statusChangeCallback(response) {
+    if (response.status === 'connected') {
+      this.setState({ isLogged: true })
+      console.log("islogged", this.state.isLogged);
+      this.testAPI();
+    }
+    else {
+      this.setState({ isLogged: false });
+      console.log("islogged", this.state.isLogged);
+      console.log("Not authenticated");
+    }
+  };
+
+  componentDidMount() {
+
     window.fbAsyncInit = () => {
       FB.init({
         appId: '175711163181508',
@@ -16,13 +31,12 @@ class FacebookLogin extends Component {
         xfbml: true,
         version: 'v2.8'
       });
-  
-      FB.getLoginStatus((response) => {
-        this.statusChangeCallback(response);
+
+      window.FB.Event.subscribe('auth.statusChange', (response) => {
+          this.statusChangeCallback(response);
       });
-  
-    };
-  
+    }
+
     (function (d, s, id) {
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) { return; }
@@ -30,40 +44,24 @@ class FacebookLogin extends Component {
       js.src = "https://connect.facebook.net/en_US/sdk.js";
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
+
   }
 
-  statusChangeCallback(response) {
-    if (response.status === 'connected') {
-      this.setState({isLogged: true})
-      console.log("islogged", this.state.isLogged)
-      this.testAPI();
-    }
-    else {
-      this.setState({isLogged: false});
-      console.log("islogged", this.state.isLogged)
-      console.log("Not authenticated");
-    }
-  }
-
-  componentDidMount() {
-    this.facebookInit();
-  }
-
-  logout(){
+  logout() {
     FB.getLoginStatus((response) => {
-      if(response && response.status === 'connected') {
+      if (response && response.status === 'connected') {
         FB.logout((response) => {
-          this.setState({isLogged: false})
+          this.setState({ isLogged: false })
           console.log(this.state.isLogged)
-       });
+        });
       }
     });
   }
 
   testAPI() {
-    FB.api('/me?fields=name,email,picture.width(160).height(160)', function(response) {
+    FB.api('/me?fields=name,email,picture.width(160).height(160)', function (response) {
       if (response && !response.error) {
-        console.log(response)
+        console.log(response);
       }
     })
   }
@@ -74,13 +72,13 @@ class FacebookLogin extends Component {
     });
   }
 
-  
+
   render() {
     return (
-    <div>
-      <div className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false" onClick={this.checkLoginState.bind(this)}></div>
-      <span onClick={this.logout.bind(this)}>logout</span>
-    </div>
+      <div>
+        <div className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false" onClick={this.checkLoginState.bind(this)}></div>
+        <span onClick={this.logout.bind(this)}>logout</span>
+      </div>
     );
   }
 

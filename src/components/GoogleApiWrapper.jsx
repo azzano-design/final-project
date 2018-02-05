db835ccb5d648a5dc12261df8fb07d8ae4f044ba
Arthur Di Lascio <arthur.dilascio@gmail.com>
2018-02-05 17:41:07 +0000

    working on filters

--- src/components/GoogleApiWrapper.jsx
+++ src/components/GoogleApiWrapper.jsx
@@ -18,53 +18,24 @@ export class MapContainer extends Component {
       menuopen: false,
       activeMarkers: [],
       address: '',
+      available_date: undefined,
+      pet_friendly: false,
+      rent_amount_min: undefined,
+      rent_amount_max: undefined,
+      water: false,
+      eletricity: false,
+      internet: false,
+      heat: false,
+      natural_gas: false,
+      storage: false,
+      laundry_on_site: false,
+      furnished: false,
+      parking: false,
       rooms: [],
-      markers: [
-
-      ],
-      otherMarkers: [
-        // {
-        //   info: '<div class="fb-logout-button"><h2>NAME</h2></div>',
-        //   label: 'Y',
-        //   title: 'hello',
-        //   latLng: { lng: -123.097665, lat: 49.264254 },
-        //   icon: {
-        //     url: '/images/icon.png'
-        //   },
-        //   availabledate: '02/03/2018'
-        // },
-
-        // {
-        //   label: 'B',
-        //   info: '- Marker2',
-        //   latLng: { lng: -123.104124, lat: 49.266416 },
-        //   icon: {
-        //     url: '/images/icon.png'
-        //   },
-        //   availabledate: '02/03/2018'
-        // },
-
-        // {
-        //   label: 'C',
-        //   info: '- Marker 3',
-        //   latLng: { lng: -123.117943, lat: 49.278747 },
-        //   icon: {
-        //     url: '/images/icon.png'
-        //   },
-        //   availabledate: '02/03/2018'
-        // },
-
-        // {
-        //   label: 'E',
-        //   info: '- Marker 5',
-        //   latLng: { lng: -123.132362, lat: 49.275147 },
-        //   icon: {
-        //     url: '/images/icon.png'
-        //   },
-        //   availabledate: '02/03/2018'
-        // }
-      ]
+      otherMarkers: []
     };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleCheckbox = this.handleCheckbox.bind(this);
   }
 
   async renderRooms() {
@@ -75,15 +46,9 @@ export class MapContainer extends Component {
         ...rooms
       ]
     })
-    for (let room of this.state.rooms){
+    for (let room of this.state.rooms) {
       this.addMarker(null, room.street, room.lat, room.lng);
     }
-  }
-
-  addMarkerFromDatabase() {
-    //TODO for each item in array this.state.rooms
-    //TODO convert this.state.rooms.street into geocoded address
-    //TODO add marker for each room
   }
 
 
@@ -140,43 +105,45 @@ export class MapContainer extends Component {
     });
   }
 
-  handleChange(event) {
-    this.setState({ address: event.target.value });
-    console.log(this.state.address);
-  }
-
-  handleSubmit(ev) {
-    ev.preventDefault();
-    console.log(ev.target.details.value);
-    console.log(ev.target.address.value);
-    console.log(this.state.address);
-
-    let newMarker = {
-      Address: ev.target.address.value,
-      Description: ev.target.details.value,
-      Cost: ev.target.cost.value,
-      Landlord: ev.target.landlord.value,
-    }
-    console.log('new marker', newMarker);
+  handleInputChange(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+    console.log("name", name);
     this.setState({
-      markers: this.state.markers.concat(newMarker)
+      [name]: value
     });
-    console.log(this.state.markers);
-
-    const geocoder = new window.google.maps.Geocoder();
-    geocoder.geocode({ address: this.state.address }, (results, status) => {
-      const location = {
-        lat: results[0].geometry.location.lat(),
-        lng: results[0].geometry.location.lng()
-      };
-      this.addMarker('Listing', '', location.lat, location.lng);
-    });
-
+    console.log("this.state.name", this.state[name]);
   }
-  onFormSubmit(event) {
-    event.preventDefault();
-    console.log(event.target.details.value);
-    console.log(event.target.address.value);
+
+  handleCheckbox(event) {
+    const name = event.target.name;
+    console.log("name", name);
+    if (this.state[name] === false) {
+      this.setState({
+        [name]: true
+      });
+    }
+    else {
+      this.setState({
+        [name]: false
+      });
+    }
+    console.log("this.state.name", this.state[name]);
+  }
+
+
+
+  async handleSubmit(ev) {
+    ev.preventDefault();
+    console.log("inside submit");
+    //  await this.setState({
+    //    rooms : []
+    //  });
+    this.state.rooms.forEach((room) => {
+      console.log("each room", room);
+    })
+    // console.log("state", this.state);
+    // console.log("rooms", this.state.rooms);
   }
 
   render() {
@@ -214,64 +181,116 @@ export class MapContainer extends Component {
           <div className="columns">
             <aside className="searchMenu column is-3">
               <button id="menu-toggle" onClick={this.toggleMenu.bind(this)}>
-                <i class="fas fa-cog"></i>
+                <i className="fas fa-cog"></i>
               </button>
               <button onClick={this.toggleDraw.bind(this)}>Polygon Search</button>
               <form onSubmit={this.handleSubmit.bind(this)}>
                 <div className="field">
                   <label>Available</label>
-                  <input type="date"></input>
+                  <input
+                    name="available_date"
+                    type="date"
+                    value={this.state.available_date}
+                    onChange={this.handleInputChange}></input>
                 </div>
                 <div className="field">
                   <label>Pet Friendly</label>
-                  <input name="pet_friendly" type="checkbox"></input>
+                  <input 
+                  name="pet_friendly" 
+                  type="checkbox"
+                  value={this.state.pet_friendly}
+                  onChange={this.handleCheckbox}></input>
                 </div>
                 <div className="field">
                   <label>Rent Amount</label>
-                  <input name="rent_amount"></input>
-                </div>
-                <div className="field">
-                  <label>Available Date</label>
-                  <input name="available_date"></input>
+                  <br />
+                  <input
+                    name="rent_amount_min"
+                    type="number"
+                    value={this.state.rent_amount_min}
+                    onChange={this.handleInputChange}
+                    placeholder="min"></input>
+                    <br />
+                  <input
+                    name="rent_amount_max"
+                    type="number"
+                    value={this.state.rent_amount_max}
+                    onChange={this.handleInputChange}
+                    placeholder="max"></input>
                 </div>
                 <div className="field">
                   <label>Water</label>
-                  <input name="water" type="checkbox"></input>
+                  <input
+                    name="water"
+                    type="checkbox"
+                    value={this.state.water}
+                    onChange={this.handleCheckbox}></input>
                 </div>
                 <div className="field">
                   <label>Eletricity</label>
-                  <input name="eletricity" type="checkbox"></input>
+                  <input
+                    name="eletricity"
+                    type="checkbox"
+                    value={this.state.eletricity}
+                    onChange={this.handleCheckbox}></input>
                 </div>
                 <div className="field">
                   <label>Internet</label>
-                  <input name="internet" type="checkbox"></input>
+                  <input
+                    name="internet"
+                    type="checkbox"
+                    value={this.state.internet}
+                    onChange={this.handleCheckbox}></input>
                 </div>
                 <div className="field">
                   <label>Heat</label>
-                  <input name="heat" type="checkbox"></input>
+                  <input
+                    name="heat"
+                    type="checkbox"
+                    value={this.state.heat}
+                    onChange={this.handleCheckbox}></input>
                 </div>
                 <div className="field">
                   <label>Natural Gas</label>
-                  <input name="natural_gas" type="checkbox"></input>
+                  <input
+                    name="natural_gas"
+                    type="checkbox"
+                    value={this.state.natural_gas}
+                    onChange={this.handleCheckbox}></input>
                 </div>
                 <div className="field">
                   <label>Storage</label>
-                  <input name="storage" type="checkbox"></input>
+                  <input
+                    name="storage"
+                    type="checkbox"
+                    value={this.state.storage}
+                    onChange={this.handleCheckbox}></input>
                 </div>
                 <div className="field">
                   <label>Onsite Laundry</label>
-                  <input name="laundry_on_site" type="checkbox"></input>
+                  <input
+                    name="laundry_on_site"
+                    type="checkbox"
+                    value={this.state.laundry_on_site}
+                    onChange={this.handleCheckbox}></input>
                 </div>
                 <div className="field">
                   <label>Fully Furnished</label>
-                  <input name="furniture" type="checkbox"></input>
+                  <input
+                    name="furnished"
+                    type="checkbox"
+                    value={this.state.furnished}
+                    onChange={this.handleCheckbox}></input>
                 </div>
                 <div className="field">
                   <label>Parking</label>
-                  <input name="parking" type="checkbox"></input>
+                  <input name="parking"
+                    type="checkbox"
+                    value={this.state.parking}
+                    onChange={this.handleCheckbox}></input>
                 </div>
                 <h1>{this.renderMarkerInfo.bind(this)()}</h1>
-                <button>Summit </button>
+                <button>Submit</button>
               </form>
             </aside>
             <div className="column is-9">
@@ -297,13 +316,17 @@ export class MapContainer extends Component {
           <div className="columns">
             <aside className="searchMenu column is-3 closed">
               <button id="menu-toggle" onClick={this.toggleMenu.bind(this)}>
-                <i class="fas fa-cog"></i>
+                <i className="fas fa-cog"></i>
               </button>
               <button onClick={this.toggleDraw.bind(this)}>Polygon Search</button>
               <form onSubmit={this.handleSubmit.bind(this)}>
                 <div className="field">
                   <label>Available</label>
-                  <input type="date"></input>
+                  <input
+                    name="available_date"
+                    type="date"
+                    value={this.state.available_date}
+                    onChange={this.handleInputChange}></input>
                 </div>
                 <div className="field">
                   <label>Pet Friendly</label>
@@ -311,50 +334,93 @@ export class MapContainer extends Component {
                 </div>
                 <div className="field">
                   <label>Rent Amount</label>
-                  <input name="rent_amount"></input>
-                </div>
-                <div className="field">
-                  <label>Available Date</label>
-                  <input name="available_date"></input>
+                  <br />
+                  <input
+                    name="rent_amount_min"
+                    type="number"
+                    value={this.state.rent_amount_min}
+                    onChange={this.handleInputChange}
+                    placeholder="min"></input>
+                  <input
+                    name="rent_amount_max"
+                    type="number"
+                    value={this.state.rent_amount_max}
+                    onChange={this.handleInputChange}
+                    placeholder="max"></input>
                 </div>
                 <div className="field">
                   <label>Water</label>
-                  <input name="water" type="checkbox"></input>
+                  <input
+                    name="water"
+                    type="checkbox"
+                    value={this.state.water}
+                    onChange={this.handleCheckbox}></input>
                 </div>
                 <div className="field">
                   <label>Eletricity</label>
-                  <input name="eletricity" type="checkbox"></input>
+                  <input
+                    name="eletricity"
+                    type="checkbox"
+                    value={this.state.eletricity}
+                    onChange={this.handleCheckbox}></input>
                 </div>
                 <div className="field">
                   <label>Internet</label>
-                  <input name="internet" type="checkbox"></input>
+                  <input
+                    name="internet"
+                    type="checkbox"
+                    value={this.state.internet}
+                    onChange={this.handleCheckbox}></input>
                 </div>
                 <div className="field">
                   <label>Heat</label>
-                  <input name="heat" type="checkbox"></input>
+                  <input
+                    name="heat"
+                    type="checkbox"
+                    value={this.state.heat}
+                    onChange={this.handleCheckbox}></input>
                 </div>
                 <div className="field">
                   <label>Natural Gas</label>
-                  <input name="natural_gas" type="checkbox"></input>
+                  <input
+                    name="natural_gas"
+                    type="checkbox"
+                    value={this.state.natural_gas}
+                    onChange={this.handleCheckbox}></input>
                 </div>
                 <div className="field">
                   <label>Storage</label>
-                  <input name="storage" type="checkbox"></input>
+                  <input
+                    name="storage"
+                    type="checkbox"
+                    value={this.state.storage}
+                    onChange={this.handleCheckbox}></input>
                 </div>
                 <div className="field">
                   <label>Onsite Laundry</label>
-                  <input name="laundry_on_site" type="checkbox"></input>
+                  <input
+                    name="laundry_on_site"
+                    type="checkbox"
+                    value={this.state.laundry_on_site}
+                    onChange={this.handleCheckbox}></input>
                 </div>
                 <div className="field">
                   <label>Fully Furnished</label>
-                  <input name="furniture" type="checkbox"></input>
+                  <input
+                    name="furniture"
+                    type="checkbox"
+                    value={this.state.furniture}
+                    onChange={this.handleCheckbox}></input>
                 </div>
                 <div className="field">
                   <label>Parking</label>
-                  <input name="parking" type="checkbox"></input>
+                  <input name="parking"
+                    type="checkbox"
+                    value={this.state.parking}
+                    onChange={this.handleCheckbox}></input>
                 </div>
                 <h1>{this.renderMarkerInfo.bind(this)()}</h1>
-                <button>Summit </button>
+                <button>Submit</button>
               </form>
             </aside>
             <div className="column is-9">

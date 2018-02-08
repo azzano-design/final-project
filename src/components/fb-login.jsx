cb50475fa756e7cfa53dfdfd56f9cd1bab12e35c
Konrad Kowalski <konradkowalski13@gmail.com>
2018-02-08 04:17:09 +0000

    base64 images seeded

--- src/components/fb-login.jsx
+++ src/components/fb-login.jsx
@@ -58,7 +58,7 @@ class FacebookLogin extends Component {
 
     window.fbAsyncInit = () => {
       FB.init({
-        appId: '175711163181508',
+        appId: '145837826089186',
         cookie: true,
         xfbml: true,
         version: 'v2.8'
